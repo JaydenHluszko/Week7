d7341c61d51403b90a1e0397d392132a4a19fa7e
JaydenHluszko <jayden.hluszko@gmail.com>
2017-11-24 13:59:50 -0500

    first

---
+++ Week 7/Week 7/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Week_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblSum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(296, 177);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(100, 20);
+            this.txtX.TabIndex = 0;
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(296, 215);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(100, 20);
+            this.txtY.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(421, 195);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lblSum
+            // 
+            this.lblSum.AutoSize = true;
+            this.lblSum.Location = new System.Drawing.Point(515, 200);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(140, 13);
+            this.lblSum.TabIndex = 3;
+            this.lblSum.Text = "i forget what to call this lmao";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 408);
+            this.Controls.Add(this.lblSum);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.txtX);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblSum;
+    }
+}
+
---
+++ Week 7/Week 7/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //capture x and y as variables
+            Int32 x = Convert.ToInt32(txtX.Text);
+            Int32 y = Convert.ToInt32(txtY.Text);
+            //send x and y to the AddNumbers method & display the result
+            Int32 result = AddNumbers(x,y);
+            lblSum.Text = Convert.ToString(result);
+        }
+
+        private Int32 AddNumbers(Int32 x, Int32 y)
+        {
+            //add the 2 input parameter values together
+            int sum = x + y;
+
+            //send back the result
+            return sum;
+        }
+    }
+}
